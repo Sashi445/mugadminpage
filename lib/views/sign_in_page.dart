68e77d4eb7d61b2012ab85a22ed1b311addfd951
charan-7 <charansurya226@gmail.com>
2021-01-10 17:29:05 +0530

    added styles to login page

--- lib/views/sign_in_page.dart
+++ lib/views/sign_in_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mugadminpage/services/firebase_auth_services.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SignInPage extends StatefulWidget {
   @override
@@ -8,7 +9,6 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-  
   final _formKey = GlobalKey<FormState>();
 
   var errorMessage = '';
@@ -18,80 +18,169 @@ class _SignInPageState extends State<SignInPage> {
   final _passwordController = TextEditingController();
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-
     final firebaseAuthServices = Provider.of<FirebaseAuthServices>(context);
 
     return Scaffold(
-        appBar: AppBar(),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(17),
-              height: MediaQuery.of(context).size.height / 2,
-              width: MediaQuery.of(context).size.width / 4,
-              decoration: BoxDecoration(
-                color: Colors.amber
-              ),
+      //appBar: AppBar(),
+      body: Row(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: Stack(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height,
+                  color: Colors.yellow,
+                  child: Image.network(
+                    'https://images.theconversation.com/files/222785/original/file-20180612-112602-1n6vzvh.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip',
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: RichText(
+                        text: TextSpan(children: [
+                      TextSpan(
+                          text: 'S',
+                          style: GoogleFonts.droidSerif(
+                              color: Colors.blue[900], fontSize: 40,fontWeight: FontWeight.w500)),
+                      TextSpan(
+                          text: 'ERV UDYAM',
+                          style: GoogleFonts.droidSerif(
+                              color: Colors.black, fontSize: 30)),
+                    ])),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width * 0.5,
+            color: Colors.amber,
+            child: Center(
               child: Form(
                 key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('ADMIN PAGE'),
-                    TextFormField(
-                      autofocus: true,
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.email), labelText: 'Email'),
-                      validator: (val) {
-                        if (val.isEmpty) {
-                          return 'This field is required!!';
-                        } else {
-                          return '';
-                        }
-                      },
-                    ),
-                    TextFormField(
-                      controller: _passwordController,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          prefixIcon: Icon(Icons.lock), labelText: 'Password'),
-                      validator: (val) {
-                        if (val.isEmpty) {
-                          return 'This field is required!!';
-                        } else {
-                          return '';
-                        }
-                      },
-                    ),
-                    Text(errorMessage, style: TextStyle(color: Colors.red)),
-                    RaisedButton(
-                      child: Text('SignIn'),
-                      onPressed: () async {
-                        var res = await firebaseAuthServices
-                            .signInWithEmailandPassword(
-                                email: _emailController.text,
-                                password: _passwordController.text);
-                        if (res == 'password-incorrect') {
-                          setState(() {
-                            errorMessage = res;
-                          });
-                        } else if (res == 'User not registered') {
-                          setState(() {
-                            errorMessage = res;
-                          });
-                        }
-                      },
-                    )
-                  ],
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 3,
+                          color: Colors.grey,
+                          offset: Offset(1, 1),
+                        )
+                      ]),
+                  height: MediaQuery.of(context).size.height * 3 / 4,
+                  padding: EdgeInsets.symmetric(vertical: 100, horizontal: 75),
+                  width: MediaQuery.of(context).size.width * 0.35,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'LOGIN',
+                          style: GoogleFonts.paytoneOne(
+                              fontSize: 30, color: Colors.black),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          autofocus: true,
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.mail),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25)),
+                              ),
+                              //prefixIcon: Icon(Icons.email),
+                              hintText: 'Email'),
+                          validator: (val) {
+                            if (val.isEmpty) {
+                              return 'This field is required!!';
+                            } else {
+                              return '';
+                            }
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                              border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(25)),
+                              ),
+                              hintText: 'Password'),
+                          validator: (val) {
+                            if (val.isEmpty) {
+                              return 'This field is required!!';
+                            } else {
+                              return '';
+                            }
+                          },
+                        ),
+                      ),
+                      Text(
+                        errorMessage,
+                        style: GoogleFonts.roboto(color: Colors.red),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: RaisedButton(
+                          color: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                                top: 10, bottom: 10, left: 20, right: 20),
+                            child: Text('Sign In',style: TextStyle(color: Colors.white,fontSize: 17.5),),
+                          ),
+                          onPressed: () async {
+                            var res = await firebaseAuthServices
+                                .signInWithEmailandPassword(
+                                    email: _emailController.text,
+                                    password: _passwordController.text);
+                            if (res == 'password-incorrect') {
+                              setState(() {
+                                errorMessage = res;
+                              });
+                            } else if (res == 'User not registered') {
+                              setState(() {
+                                errorMessage = res;
+                              });
+                            }
+                          },
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-              )),
-        ));
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
